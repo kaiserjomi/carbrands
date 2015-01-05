3cc263e27875cf978cdc556cf08b7dfa84197a99
Kaiser <jomi.man@gmail.com>
2015-01-05 13:22:49 +0000

    alert no brands

--- carbrands.js
+++ carbrands.js
@@ -1,23 +1,43 @@
+var okCancelEvents = function (selector, callbacks) {
+  var ok = callbacks.ok || function() {};
+  var cancel = callbacks.cancel || function() {};
+  
+  var events = {};
+  events['keyup '+selector+', keydown '+selector+', focusout '+selector]=
+    function (evt) {
+    if(evt.type === "keydown" && evt.which === 27){
+      //escape = cancel
+      cancel.call(this, evt);
+    }else if (evt.type === "keyup" && evt.which === 13){
+      //blur/return/enter = ok/submit if non-empty
+      var value = String(evt.target.value || "");
+      if (value)
+        ok.call(this, value, evt);
+      else
+        cancel.call(this, evt);
+    }
+  };
+  
+  return events;
+};
+
+
+Carbrands = new Meteor.Collection('carbrands');
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
-    }
-  });
+  Template.main.carbrands = function() {
+    return Carbrands.find();
+  }
+  
+  Template.main.events(okCancelEvents(
+  '#brand',
+  {
+  ok: function (text, evt) {
+    alert(text);
+  }
+  }));
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+
 }
